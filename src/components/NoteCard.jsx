0795ddc48195d0dfbba64b3f517f00d719f9ce13
leanghok120 <leanghokoeng5@gmail.com>
2024-11-24 07:34:52 +0700

    Add header for note

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { Trash2 } from "lucide-react";
 
 function NoteCard({ note }) {
   const body = JSON.parse(note.body);
   const colors = JSON.parse(note.colors);
   const postion = JSON.parse(note.position);
 
+  const textAreaRef = useRef(null);
+
+  function adjustNoteHeight() {
+    const textarea = textAreaRef.current;
+    textarea.style.height = "auto";
+    textarea.style.height = `${textarea.scrollHeight}px`;
+  }
+
+  useEffect(() => {
+    adjustNoteHeight();
+  }, []);
+
   return (
     <div
-      className="rounded-2xl p-5 w-96 absolute"
+      className="rounded-2xl w-96 absolute"
       style={{
         backgroundColor: colors.colorBody,
         color: colors.colorText,
@@ -15,13 +28,22 @@ function NoteCard({ note }) {
         top: `${postion.y}px`,
       }}
     >
-      <textarea
-        id="body"
-        name="body"
-        className="bg-inherit focus:outline-none resize-none text-xl w-full"
+      <div
+        className="p-2 rounded-tl-2xl rounded-tr-2xl"
+        style={{ backgroundColor: colors.colorHeader }}
       >
-        {body}
-      </textarea>
+        <Trash2 />
+      </div>
+      <div className="p-5">
+        <textarea
+          ref={textAreaRef}
+          id="body"
+          name="body"
+          className="bg-inherit focus:outline-none resize-none text-xl w-full"
+          defaultValue={body}
+          onInput={adjustNoteHeight}
+        ></textarea>
+      </div>
     </div>
   );
 }
